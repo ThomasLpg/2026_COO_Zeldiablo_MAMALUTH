65ef3f159f2becfbe586fb834ba352aba0d2282d
Matteo Scheltien <matteo.scheltien1@etu.univ-lorraine.fr>
2026-06-03 16:04:42 +0200

    Javadoc des items

--- src/items/Epee.java
+++ src/items/Epee.java
@@ -6,14 +6,28 @@ public class Epee extends Item{
 
     private int degats;
 
+    /**
+     *  Constructeur de l'item épée
+     * @param x
+     * @param y
+     * @param d
+     */
     public Epee(int x, int y, int d){
         super(x, y);
         this.degats = d;
     }
 
+    /**
+     * Ajoute aux dégats de base du héros les dégats de l'épée
+     * @param h le héro
+     */
     public void affecter(Hero h){
        h.addDegats(this.degats);
     }
 
+    /**
+     * Getter des dégats l'épée
+     * @return int dégats
+     */
     public int getDegats(){return this.degats;}
 }
--- src/items/Item.java
+++ src/items/Item.java
@@ -10,17 +10,39 @@ public abstract class Item {
     private int x;
     private int y;
 
+    /**
+     * Constructeur d'un item
+     * @param x
+     * @param y
+     */
     public Item(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Getter coordonnée x de l'item
+     * @return x
+     */
     public int getX(){return this.x;}
 
+    /**
+     * Getter coordonnée y de l'item
+     * @return y
+     */
     public int getY(){return this.y;}
 
+    /**
+     * Gère le comportement de l'item
+     * @param h
+     */
     public abstract void affecter(Hero h);
 
+    /**
+     * Permet de dessiner un item
+     * @param g
+     * @param c
+     */
     public void dessiner(Graphics2D g, Color c){
         g.setColor(c);
         //ajouter le sprite
