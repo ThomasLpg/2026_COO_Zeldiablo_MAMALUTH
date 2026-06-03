49dbe3d5e79dbdbea43d20df9491806e22edba7b
Matteo Scheltien <matteo.scheltien1@etu.univ-lorraine.fr>
2026-06-04 00:16:27 +0200

    Ajout des sprites de Hero et Monstre et de la méthode qui les dessine

--- src/personnage/DessinPerso.java
+++ src/personnage/DessinPerso.java
@@ -1,20 +1,22 @@
 package personnage;
-
 import Roles.Hero;
 import Roles.Monstre;
 import moteurJeu.DessinJeu;
 
-
-import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class DessinPerso implements DessinJeu {
     ArrayList<Personnage> listePerso;
 
     public static final int TAILLE = 20;
     JeuPerso jeu;
+    private BufferedImage imgHero;
+    private BufferedImage imgMonstre;
 
     /**
      * Permet de définir le personnage à dessiner
@@ -24,24 +26,32 @@ public class DessinPerso implements DessinJeu {
     public DessinPerso(JeuPerso j, ArrayList<Personnage> listePerso){
         this.jeu = j;
         this.listePerso = listePerso;
+
+        try{
+            imgHero = ImageIO.read(new File("image/hero.png"));
+            imgMonstre = ImageIO.read(new File("image/monstre.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Appelle la méthode dessiner de Dessins pour dessiner le personnage
+     * Dessine les personnages grace aux sprites donnés
      * @param image
      */
     public void dessiner(BufferedImage image){
         Graphics2D g = (Graphics2D) image.getGraphics();
         for (Personnage p : this.listePerso){
-            if(p instanceof Hero){
-                p.dessiner(g, Color.BLUE);
-            } else if (p instanceof Monstre) {
-                p.dessiner(g, Color.RED);
+            int x = p.getX() * TAILLE;
+            int y = p.getY() * TAILLE;
+
+            if (p instanceof Hero){
+                g.drawImage(imgHero, x, y, TAILLE, TAILLE, null);
+
+            } else if (p instanceof Monstre){
+                g.drawImage(imgMonstre, x, y, TAILLE, TAILLE, null);
             }
         }
-
         g.dispose();
-
-
     }
 }
